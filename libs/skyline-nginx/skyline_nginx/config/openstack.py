5e4f40f364d49bd2cf36c4c2a22eb45205823b40
Gao Hanxiang <gao.hanxiang@99cloud.net>
2021-07-18 14:27:40 -0400

    feat: Add skyline-nginx package
    1. Add skyline-nginx package for managing and generating nginx
       configuration.
    
    Change-Id: I68afd9ab9ad52fc96c13e745cb1e89a8061a53ba

---
+++ libs/skyline-nginx/skyline_nginx/config/openstack.py
@@ -0,0 +1,44 @@
+# Copyright 2021 99cloud
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from skyline_apiserver.config.openstack import (
+    default_region,
+    interface_type,
+    keystone_url,
+    nginx_prefix,
+    service_mapping,
+    system_project,
+    system_project_domain,
+    system_user_domain,
+    system_user_name,
+    system_user_password,
+)
+
+GROUP_NAME = __name__.split(".")[-1]
+ALL_OPTS = (
+    default_region,
+    keystone_url,
+    system_project_domain,
+    system_project,
+    system_user_domain,
+    system_user_name,
+    system_user_password,
+    interface_type,
+    nginx_prefix,
+    service_mapping,
+)
+
+__all__ = ("GROUP_NAME", "ALL_OPTS")
