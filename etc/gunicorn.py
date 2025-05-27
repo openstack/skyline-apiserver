65673a38f330dd79eb7bcbfd97a8f0910bf15156
Wenxiang Wu <wu.wenxiang@99cloud.net>
2025-05-28 06:51:51 +0800

    fix: Update pydantic constraint & gunicorn config
    This is to make it compatible with fastapi 0.58.1
    
    Also add some hints to type checker to make it
    happy about the default values of some pydantic
    objects.
    
    Gunicorn config should update since new linux kernel
    
    Closes-Bug: #2111808
    
    Change-Id: I468e3377639bb28c41cf1500ee8933eb1ed9bfb0

--- etc/gunicorn.py
+++ etc/gunicorn.py
@@ -15,11 +15,13 @@
 import multiprocessing
 
 bind = "unix:/var/lib/skyline/skyline.sock"
+reuse_port = False
+# bind = "0.0.0.0:8000"
+# reuse_port = True
 workers = (1 + multiprocessing.cpu_count()) // 2
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 300
 keepalive = 5
-reuse_port = True
 proc_name = "skyline"
 
 logconfig_dict = {
