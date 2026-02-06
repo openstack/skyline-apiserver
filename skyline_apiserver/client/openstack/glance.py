openstack/skyline-apiserver

commit cd31aa512728564b7efa1c1f06a8f8c6d1d9f643

Replace glanceclient with openstacksdk
The glance client will be deprecated one day. Projects using it should
switch to using the OpenStack SDK instead.

Also add "attrs" to requirements.txt. It was previously pulled through
python-glanceclient.

Change-Id: I35c598d318348c2b14323ea29c8347312733f8bc
Signed-off-by: Cyril Roelandt <cyril@redhat.com>

skyline_apiserver/client/openstack/glance.py
@@ -35,12 +35,12 @@ def list_images(
         kwargs = {}
         if filters:
             kwargs["filters"] = filters
-        gc = utils.glance_client(
+        ic = utils.image_client(
             session=session,
             region=profile.region,
             global_request_id=global_request_id,
         )
-        return gc.images.list(**kwargs)
+        return ic.images.images(**kwargs)
     except Unauthorized as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,

skyline_apiserver/client/utils.py
@@ -16,8 +16,8 @@ from __future__ import annotations
 
 from typing import Any, Optional
 
+import openstack
 from cinderclient.client import Client as CinderClient
-from glanceclient.client import Client as GlanceClient
 from keystoneauth1.access.access import AccessInfoV3
 from keystoneauth1.identity.v3 import Password, Token
 from keystoneauth1.session import Session
@@ -114,15 +114,13 @@ def keystone_client(
     return client
 
 
-def glance_client(
+def image_client(
     session: Session,
     region: str,
     global_request_id: Optional[str] = None,
-    version: str = constants.GLANCE_API_VERSION,
 ) -> HTTPClient:
     endpoint = get_endpoint(region, "image", session=session)
-    client = GlanceClient(
-        version=version,
+    client = openstack.connection.Connection(
         session=session,
         endpoint=endpoint,
         global_request_id=global_request_id,

skyline_apiserver/types/constants.py
@@ -18,7 +18,6 @@ ALGORITHM = "HS256"
 
 KEYSTONE_API_VERSION = "3.13"
 NOVA_API_VERSION = "2.79"
-GLANCE_API_VERSION = "2"
 CINDER_API_VERSION = "3.59"
 NEUTRON_API_VERSION = "2.0"
 

commit e3f999a4224a25c6c1d4e8425db082fff836a583

Fix typo in list_images()
This was introduced in cd31aa512728564b7efa1c1f06a8f8c6d1d9f643 .

Thanks to Lennart Nordgreen for reporting this issue.

Change-Id: I628f4b6045f3368e4cc9af5599d139c2a51b3720
Signed-off-by: Cyril Roelandt <cyril@redhat.com>

skyline_apiserver/client/openstack/glance.py
@@ -40,7 +40,7 @@ def list_images(
             region=profile.region,
             global_request_id=global_request_id,
         )
-        return ic.images.images(**kwargs)
+        return ic.image.images(**kwargs)
     except Unauthorized as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,