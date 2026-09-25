082401597d7c60771596afe8849663381112aa93
Wu Wenxiang <wu.wenxiang@algoblu.com>
2026-09-25 11:54:39 +0800

    Cleanup zaqar service registration and drop weak policy tests
    Keep SUPPORTED_SERVICE_EPS alphabetically ordered after the zaqar
    addition, and remove the lightweight registration/prefix smoke tests
    that other services do not carry.
    
    Change-Id: I35cefdc308c0a47d2022109353fa6f9dcaf4560f
    Signed-off-by: Wu Wenxiang <wu.wenxiang@algoblu.com>

--- skyline_apiserver/types/constants.py
+++ skyline_apiserver/types/constants.py
@@ -50,7 +50,6 @@ SUPPORTED_SERVICE_EPS = {
     # openstack_service: [<entry_point_name>, <entry_point_name>,]
     "barbican": ["barbican"],
     "cinder": ["cinder"],
-    "zaqar": ["zaqar"],
     "designate": ["designate"],
     "glance": ["glance"],
     "heat": ["heat"],
@@ -64,6 +63,7 @@ SUPPORTED_SERVICE_EPS = {
     "octavia": ["octavia"],
     "placement": ["placement"],
     "trove": ["trove"],
+    "zaqar": ["zaqar"],
     "zun": ["zun"],
 }
 
